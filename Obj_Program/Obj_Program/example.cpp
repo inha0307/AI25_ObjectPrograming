f9bb6ae19618cd824f4fc1938dfd7f604393b6c2
inha0307 <emforhs033@naver.com>
2025-09-09 15:39:26 +0900

    0909_03

---
+++ Obj_Program/Obj_Program/example.cpp
@@ -0,0 +1,13 @@
+// 두 번째 예제 프로그램
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int x, y;
+	cout << "이름을 입력하시오: ";
+	cin >> x >> y;
+	cout << x << y << endl;
+	return 0;
+}
